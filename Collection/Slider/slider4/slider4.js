b194b1e3e3cee7edbb567d936424b731e6e78ca6
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-07-25 14:18:42 -0300

    Update slider4.js

--- Collection/Slider/slider4/slider4.js
+++ Collection/Slider/slider4/slider4.js
@@ -1,1 +1,29 @@
+ const swiperElement = document.querySelector('[data-identifier="slider-side"]');
+  const nextEl = swiperElement.querySelector('[data-identifier="side-next-slide"]');
+  const prevEl = swiperElement.querySelector('[data-identifier="side-prev-slide"]');
+  
+  const swiper1 = new Swiper(swiperElement, {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 3,
+    spaceBetween: 16,
+    centeredSlides: false,
 
+    // Navigation arrows
+    navigation: {
+      nextEl: nextEl,
+      prevEl: prevEl,
+    },
+
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      568: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+    },
+  });
