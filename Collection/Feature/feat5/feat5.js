46a54fe5fcc4c63525423b686a5027b41635103f
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-07-31 12:49:04 -0300

    Create feat5.js

---
+++ Collection/Feature/feat5/feat5.js
@@ -0,0 +1,27 @@
+    var qrcode;
+
+    function createQR() {
+        var qrcodeDivs = document.querySelectorAll('[data-identifier="feature-qrcode"]');
+        
+        qrcodeDivs.forEach(function(qrcodeDiv) {
+            var url = qrcodeDiv.getAttribute('data-qr-code');
+            
+            // Get color attributes from the div
+            var colorLight = qrcodeDiv.getAttribute('data-color-light') || "#ffffff";
+            var colorDark = qrcodeDiv.getAttribute('data-color-dark') || "#000000";
+
+            // Clear any existing QR codes in the container
+            qrcodeDiv.innerHTML = "";
+            
+            // Create a new QR code
+            qrcode = new QRCode(qrcodeDiv, {
+                text: url,
+                colorLight: colorLight,
+                colorDark: colorDark,
+            });
+        });
+    }
+
+    window.onload = function() {
+        createQR();
+    }
