2a1d2855132980e6e93705bdabe781369481ddd2
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-06 07:15:08 -0300

    Update text1.js

--- Collection/Text/text1/text1.js
+++ Collection/Text/text1/text1.js
@@ -1,41 +1,42 @@
-  // function to create a unique cursor style and append to document head
-  function makeCursorStyle(typedElement, cursorSize) {
-    const uniqueClassName = 'typed-cursor-' + typedElement.dataset.identifier.replace(/\s+/g, '-').toLowerCase();
-    const style = document.createElement('style');
-    style.innerHTML = `.${uniqueClassName} { 
-        font-size: ${cursorSize}; 
-        vertical-align: middle;
-    }`;
-    document.head.appendChild(style);
-    return uniqueClassName;
+ // function to create a unique cursor style and append to document head
+function makeText1CursorStyle(typedElement, cursorSize) {
+  const uniqueText1ClassName = 'typedCursor-' + typedElement.dataset.identifier.replace(/\s+/g, '-').toLowerCase();
+  const text1Style = document.createElement('style');
+  text1Style.innerHTML = `.${uniqueText1ClassName} { 
+      font-size: ${cursorSize}; 
+      vertical-align: middle;
+  }`;
+  document.head.appendChild(text1Style);
+  return uniqueText1ClassName;
+}
+
+function getText1Sentences(typedElement) {
+  let text1Sentences = [];
+  let i = 1;
+  while(typedElement.hasAttribute(`data-basic-typing-sentence${i}`)) {
+    text1Sentences.push(typedElement.getAttribute(`data-basic-typing-sentence${i}`));
+    i++;
   }
+  return text1Sentences;
+}
 
-  function getSentences(typedElement) {
-    let sentences = [];
-    let i = 1;
-    while(typedElement.hasAttribute(`data-basic-typing-sentence${i}`)) {
-      sentences.push(typedElement.getAttribute(`data-basic-typing-sentence${i}`));
-      i++;
-    }
-    return sentences;
-  }
+const typedText1Elements = document.querySelectorAll('[data-identifier="basic-typing"]');
+typedText1Elements.forEach(typedText1 => {
+  const typedText1SpeedValue = parseFloat(typedText1.getAttribute('data-typing-speed'));
+  const typedText1CursorSize = typedText1.getAttribute('data-type-cursor-size');
+  const text1CursorClass = makeText1CursorStyle(typedText1, typedText1CursorSize);
+  const text1LoopValue = typedText1.getAttribute('data-basic-typing-loop') === 'true' ? true : false; // Added line
 
-  const typedTexts1 = document.querySelectorAll('[data-identifier="basic-typing"]');
-  typedTexts1.forEach(typedText => {
-    const typedSpeedValue = parseFloat(typedText.getAttribute('data-typing-speed'));
-    const typedCursorSize = typedText.getAttribute('data-type-cursor-size');
-    const cursorClass = makeCursorStyle(typedText, typedCursorSize);
-    const loopValue = typedText.getAttribute('data-basic-typing-loop') === 'true' ? true : false; // Added line
+  const text1Sentences = getText1Sentences(typedText1);
 
-    const sentences = getSentences(typedText);
-
-    const typed1 = new Typed(typedText, {
-      strings: sentences,
-      typeSpeed: typedSpeedValue,
-      cursorChar: `<span class="${cursorClass}">|</span>`,
-      loop: loopValue, // Used the value of the new attribute
-    });
+  const text1Typed = new Typed(typedText1, {
+    strings: text1Sentences,
+    typeSpeed: typedText1SpeedValue,
+    cursorChar: `<span class="${text1CursorClass}">|</span>`,
+    loop: text1LoopValue, // Used the value of the new attribute
   });
+});
+
 
 //this Mosaico element uses typed.js (License below)
 
