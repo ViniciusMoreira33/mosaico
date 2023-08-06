3e2d83acd23ad13a36fdccee66ab800f58913990
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-06 07:56:33 -0300

    Update btn8.js

--- Collection/Buttons/btn8/btn8.js
+++ Collection/Buttons/btn8/btn8.js
@@ -1,17 +1,17 @@
+<script>
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelector('[data-identifier="twitter-share"]').addEventListener("click", function(event) {
         event.preventDefault();
 
         // Fetches values from data attributes
         var shareMessage = this.getAttribute('data-share-message');
-        var shareUrl = this.getAttribute('data-share-url');
 
         // Custom Twitter share URL
         var url = "https://twitter.com/intent/tweet?";
         url += "text=" + encodeURIComponent(shareMessage); // share text from data attribute
-        url += "&url=" + encodeURIComponent(shareUrl); // URL from data attribute
 
         // Open new window with the Twitter sharing dialog
         window.open(url, '_blank');
     });
 });
+</script>
