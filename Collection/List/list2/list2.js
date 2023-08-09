4be769e0c3de54f2704a535f954c24fbc1a7fa33
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-09 13:35:21 -0300

    Create list2.js

---
+++ Collection/List/list2/list2.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var list2 = document.querySelector('[data-identifier="list-reorderable"]');
+
+  var placeholderBgColor = list2.getAttribute('data-reorderable-placeholder-bg-color');
+  var placeholderBorderColor = list2.getAttribute('data-reorderable-placeholder-border');
+  var placeholderColor = list2.getAttribute('data-reorderable-placeholder-color');
+
+  var placeholderStyles = `
+    background-color: ${placeholderBgColor};
+    border: ${placeholderBorderColor};
+    color: ${placeholderColor};
+  `;
+
+  var styleSheet = document.createElement('style');
+  styleSheet.type = 'text/css';
+  styleSheet.innerHTML = `.reorderable--placeholder { ${placeholderStyles} }`;
+  document.head.appendChild(styleSheet);
+
+  new Sortable(list2, {
+    animation: parseInt(list2.getAttribute('data-reorderable-animation')),
+    ghostClass: 'reorderable--placeholder',
+  });
+});
+
+
+
+//This Mosaico element uses Sortable.js (License below)
+
+/*
+Copyright (c) 2019 All contributors to Sortable
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
