f1b91c8969588e61a14cb84129bf29fa5daf8e9c
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-16 12:16:21 -0300

    Update feat2.js

--- Collection/Feature/feat2/feat2.js
+++ Collection/Feature/feat2/feat2.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2023 Mosaico
+ * 
+ * This code is part of the Mosaico's Collection and is subject to the terms of the Mosaico Software License Agreement.
+ * Unauthorized use, copying, modification, or distribution of this code without valid permission is strictly prohibited.
+ * 
+ * For licensing information, please refer to the Mosaico Software License Agreement available at www.mosaico.site/privacy-policy-and-terms
+ */
+
 var feat2Clipboard = new ClipboardJS('[data-clipboard-target]');
 
 feat2Clipboard.on('success', function(feat2Event) {
