bd6b8fa13e49b50e151b6d85d05e6fc55e3bee35
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-16 12:11:27 -0300

    Update btn3.js

--- Collection/Buttons/btn3/btn3.js
+++ Collection/Buttons/btn3/btn3.js
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2023 Mosaico
+ * 
+ * This code is part of the Mosaico's Collection and is subject to the terms of the Mosaico Software License Agreement.
+ * Unauthorized use, copying, modification, or distribution of this code without valid permission is strictly prohibited.
+ * 
+ * For licensing information, please refer to the Mosaico Software License Agreement available at www.mosaico.site/privacy-policy-and-terms
+ */
+
 (function() {
   const btn3Buttons = document.querySelectorAll("[data-identifier='button-position-aware']");
 
