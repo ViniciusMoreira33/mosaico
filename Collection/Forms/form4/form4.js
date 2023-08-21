71f6bb044daf6100433ac38adca730851f0e0c72
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-21 09:36:31 -0300

    Update form4.js

--- Collection/Forms/form4/form4.js
+++ Collection/Forms/form4/form4.js
@@ -7,44 +7,43 @@
  * For licensing information, please refer to the Mosaico Software License Agreement available at www.mosaico.site/privacy-policy-and-terms
  */
 
-// Wait for the page to load completely
 document.addEventListener('DOMContentLoaded', function() {
     // Handle checkboxes
-    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(function(checkbox) {
-        checkbox.addEventListener('change', function() {
-            var buttonElement = checkbox.closest('[data-identifier="checkbox-button"]');
-            var bgColor = buttonElement.getAttribute('data-checkbox-active-bg');
-            var textColor = buttonElement.getAttribute('data-checkbox-active-text');
+    var form4Checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    form4Checkboxes.forEach(function(form4Checkbox) {
+        form4Checkbox.addEventListener('change', function() {
+            var form4ButtonElement = form4Checkbox.closest('[data-identifier="checkbox-button"]');
+            var form4BgColor = form4ButtonElement.getAttribute('data-checkbox-active-bg');
+            var form4TextColor = form4ButtonElement.getAttribute('data-checkbox-active-text');
             
-            if (checkbox.checked) {
-                if (bgColor) buttonElement.style.backgroundColor = bgColor;
-                if (textColor) buttonElement.style.color = textColor;
+            if (form4Checkbox.checked) {
+                if (form4BgColor) form4ButtonElement.style.backgroundColor = form4BgColor;
+                if (form4TextColor) form4ButtonElement.style.color = form4TextColor;
             } else {
-                buttonElement.style.backgroundColor = '';
-                buttonElement.style.color = '';
+                form4ButtonElement.style.backgroundColor = '';
+                form4ButtonElement.style.color = '';
             }
         });
     });
 
     // Handle radio buttons
-    var radioButtons = document.querySelectorAll('input[type="radio"]');
-    radioButtons.forEach(function(radioButton) {
-        radioButton.addEventListener('change', function() {
+    var form4RadioButtons = document.querySelectorAll('input[type="radio"]');
+    form4RadioButtons.forEach(function(form4RadioButton) {
+        form4RadioButton.addEventListener('change', function() {
             // Reset styles for all radio buttons in the same group
-            var sameGroup = document.querySelectorAll('input[type="radio"][name="' + radioButton.name + '"]');
-            sameGroup.forEach(function(radio) {
-                var buttonElement = radio.closest('[data-identifier="radio-button"]');
-                buttonElement.style.backgroundColor = '';
-                buttonElement.style.color = '';
+            var form4SameGroup = document.querySelectorAll('input[type="radio"][name="' + form4RadioButton.name + '"]');
+            form4SameGroup.forEach(function(form4Radio) {
+                var form4ButtonElement = form4Radio.closest('[data-identifier="radio-button"]');
+                form4ButtonElement.style.backgroundColor = '';
+                form4ButtonElement.style.color = '';
             });
             
             // Apply styles to the selected radio button
-            var buttonElement = radioButton.closest('[data-identifier="radio-button"]');
-            var bgColor = buttonElement.getAttribute('data-radio-active-bg');
-            var textColor = buttonElement.getAttribute('data-radio-active-text');
-            if (bgColor) buttonElement.style.backgroundColor = bgColor;
-            if (textColor) buttonElement.style.color = textColor;
+            var form4ButtonElement = form4RadioButton.closest('[data-identifier="radio-button"]');
+            var form4BgColor = form4ButtonElement.getAttribute('data-radio-active-bg');
+            var form4TextColor = form4ButtonElement.getAttribute('data-radio-active-text');
+            if (form4BgColor) form4ButtonElement.style.backgroundColor = form4BgColor;
+            if (form4TextColor) form4ButtonElement.style.color = form4TextColor;
         });
     });
 });
