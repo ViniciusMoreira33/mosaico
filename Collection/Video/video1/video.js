83eb5ffd757cd6fc2321343f96bc3edde73b4353
ViniciusMoreira33 <87245743+ViniciusMoreira33@users.noreply.github.com>
2023-08-22 13:02:16 -0300

    Update video.js

--- Collection/Video/video1/video.js
+++ Collection/Video/video1/video.js
@@ -1,4 +1,11 @@
-
+/*
+ * Copyright (c) 2023 Mosaico
+ * 
+ * This code is part of the Mosaico's Collection and is subject to the terms of the Mosaico Software License Agreement.
+ * Unauthorized use, copying, modification, or distribution of this code without valid permission is strictly prohibited.
+ * 
+ * For licensing information, please refer to the Mosaico Software License Agreement available at www.mosaico.site/privacy-policy-and-terms
+ */
 
 const videoElements = document.querySelectorAll('[data-identifier="embed-player"]');
 
@@ -156,3 +163,17 @@ videoElements.forEach((element) => {
       src: getAttributeWithDefault(videoElements[0], 'data-embed-player-previewThumbnails-src', ''),
     },
   });
+
+
+/*This Mosaico element uses plyr.io (License below)*/
+/*
+The MIT License (MIT)
+
+Copyright (c) 2017 Sam Potts
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
